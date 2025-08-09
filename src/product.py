d75b7a7ff4da4b07da7170247312baadfc9f148e
Kseniya <baharevaxen@yandex.ru>
2025-08-09 23:02:31 +0300

    creating casses and tests

---
+++ src/product.py
@@ -0,0 +1,32 @@
+class Product:
+    """Создаем класс Product"""
+    # Атрибут класса который считает количество продуктов
+    product_count = 0
+    def __init__(self, name: str, description: str, quantity: int, price:float):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        self.price = price
+        Product. product_count += 1 # Добавляем +1 Product
+
+
+class Category:
+    """Создаем класс Category"""
+    # атрибут класса считает количество категорий
+    category_count = 0
+    def __init__(self, name: str, description: str, products: list):
+        self.name = name
+        self.description = description
+        self.products = products
+        Category.category_count += 1 # Добавляем +1 Категорию
+
+product1 = Product("banana", "Only big size", 150, 174.50 )
+product2 = Product("apple", "Only red", 200, 67.80)
+
+category = Category("fruit", "import", [product1,product2])
+
+print(Product. product_count)
+print(Category.category_count)
+
+
+
---
+++ tests/test_class.py
@@ -0,0 +1,41 @@
+import pytest
+from src.product import Product
+from src.product import Category
+
+
+@pytest.fixture
+def class_product():
+    return Product("banana", "Only big size", 150, 174.50 )
+
+
+def test_product(class_product):
+    assert class_product.name == "banana"
+    assert class_product.description == "Only big size"
+    assert class_product.quantity == 150
+    assert class_product.price == 174.50
+
+
+@pytest.fixture
+def class_category():
+    return Category("fruit", "import", ["product1","product2"])
+
+
+def test_category(class_category):
+    assert class_category.name == "fruit"
+    assert class_category.description == "import"
+    assert class_category.products == ["product1","product2"]
+
+
+def test_product_count():
+    Product.product_count = 0
+    product1 = Product("banana", "Only big size", 150, 174.50)
+    product2 = Product("apple", "Only red", 200, 67.80)
+    assert Product.product_count == 2
+
+
+def test_category_count():
+    Category.category_count = 0
+    category = Category("fruit", "import", ["product1", "product2"])
+    assert Category.category_count == 1
+
+
