00b47517c9aa5cc9eca6a366b345837f3980134f
Kseniya <baharevaxen@yandex.ru>
2025-08-11 12:03:55 +0300

    creating main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from src. product import Product, Category
+
+""" Исполняемый код из product.py"""
+product1 = Product("banana", "Only big size", 150, 174.50)
+product2 = Product("apple", "Only red", 200, 67.80)
+product3 = Product("orange", "Argentina", 80, 95.00)
+
+
+category = Category("fruit", "import", [product1, product2, product3])
+
+
+print("Новое количество продуктов в категории:", len(category.products))
+print("Общее количество продуктов:", Category.product_count)
+print("Количество категорий:", Category.category_count)
